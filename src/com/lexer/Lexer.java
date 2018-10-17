3c6b5b276f36b346e03e8af3b196f0b9e6f04485
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2018-10-17 11:52:51 +0200

    Recognize Relops, identificators and keyword

--- src/com/lexer/Lexer.java
+++ src/com/lexer/Lexer.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
+import com.sd.KeyWordTbl;
 import com.sd.Token;
 
 
@@ -15,35 +18,32 @@ public class Lexer {
     
 	private final int POZZO = 100;
     private TreeMap<Integer, String> symbTbl;
-    
-    private  char begin;
+    private KeyWordTbl keyWord;
     private  char forward;
     private static boolean keep;
+    private static Integer key;
+    
     public Lexer() {
     	this.symbTbl = new TreeMap<>();
-    	this.begin = '\0';
+    	this.keyWord = new KeyWordTbl();
     	this.forward = '\0';
     	this.keep=false;
-
+    	this.key=0;
     }
 
     public Token getToken(InputStream fr) throws IOException {
-        Token toReturn = new Token("INVALID","TOKEN");
-
-        String id = "";
+        Token toReturn = new Token();
+        String str = "";
         int state = 0;
-        if(!keep) {begin=(char) fr.read();
-        forward = begin;
-        }
+        if(!keep) forward=(char) fr.read();
         keep=false;
-        
         while (true) {
             switch (state) {
             case 0: 
             		if(forward == '<') {state=1;
             							forward=(char) fr.read();
             							}
-            		else if(forward == '=') state=5;
+            		else if(forward == '=') {state=5;}
             		else if(forward == '>') {state=6;
             								 forward=(char) fr.read();
             								 }
@@ -52,7 +52,8 @@ public class Lexer {
             case 1: if(forward == '=') state=2;
             		else if(forward == '>') state=3;
             		else if(forward != '=' || forward != '>'){state=4;
-            			  				     				  keep=true;}
+            			  				     				  keep=true;
+            			  				     				  }
             		else state=POZZO;
             		break;
             case 2: toReturn.setClasse("RELOP");
@@ -68,9 +69,11 @@ public class Lexer {
     				toReturn.setLessema("EQ");
     				return toReturn;
             case 6: if(forward == '=') {state=7;
+        
             							}
             		else if(forward != '='){state=8;
-            			  					keep=true;}
+            		keep=true;
+            			  					}
             		else state=POZZO;
             		break;
             case 7: toReturn.setClasse("RELOP");
@@ -79,11 +82,56 @@ public class Lexer {
             case 8: toReturn.setClasse("RELOP");
     				toReturn.setLessema("GT");
     				return toReturn;
-            case POZZO: forward=(char) fr.read();
-            			keep=true;
-            			state=0;
+            case 9: if(Character.isLetter(forward)) {state=10;
+            										 str+=forward;
+            										 forward=(char) fr.read();
+            										 }
+            		else state=POZZO;
+            		break;
+            case 10: if(Character.isLetterOrDigit(forward)) {str+=forward;
+            												forward=(char) fr.read();
+            												 }
+            		 else if(!Character.isLetterOrDigit(forward)){state=11;
+            		 											  keep=true;}
+            		 else state=POZZO;
+            		break;
+            case 11: 
+            		if(keyWord.containsValue(str)) { toReturn.setClasse(str.toUpperCase());
+            										 toReturn.setLessema(str);
+            										 return toReturn;
+            										 }
+            		 else {
+            			   if(!symbTbl.containsValue(str)) {
+            				   							  System.out.println("VUOTA O NON CONTIENE");
+            				   							  symbTbl.put(key, str);
+            				   							  toReturn.setClasse(key.toString());
+            				 							  toReturn.setLessema(str);
+            				 							  key++;
+            				 							  return toReturn;
+            			 								 }
+            			 else {
+            				 System.out.println("CONTIENE");
+            				 Set<Entry<Integer,String>>list=symbTbl.entrySet();
+            				 for(Entry entry:list) {
+            					 String value= (String) entry.getValue();
+            					 if(value.compareTo(str)==0) {Integer k=(Integer) entry.getKey();
+            						 						  toReturn.setClasse(""+k);
+            						 						  toReturn.setLessema(str);
+            						 						  return toReturn;}
+            				 }
+            			 }
+            		 }
+            		break;
+            case POZZO: 
+            			if(forward == '='||forward == '>'|| forward == '<') state=0;
+            			else if(Character.isLetter(forward)) state=9;
+            			//else if(Character.isDigit(forward)) state=<todo>;
+            			else {forward=(char) fr.read();
+            			
+            			}
             			break;
             }
+            
         	
         }//fine while
     }
--- src/com/sd/KeyWordTbl.java
+++ src/com/sd/KeyWordTbl.java
@@ -2,22 +2,14 @@ package com.sd;
 
 import java.util.TreeMap;
 
-public class KeyWordTbl {
-
-	private TreeMap<String,String> keyWord;
+public class KeyWordTbl extends TreeMap<String,String> {
 	
 	public KeyWordTbl() {
-		this.keyWord = new TreeMap<>();
-		keyWord.put("IF", "if");
-		keyWord.put("THEN", "then");
-		keyWord.put("ELSE", "else");
-		keyWord.put("INT", "int");
+		put("IF", "if");
+		put("THEN", "then");
+		put("ELSE", "else");
+		put("INT", "int");
 	}
 	
-	public Token search(String key) {
-		String value;
-		value=this.keyWord.get(key);
-		Token toReturn = new Token(key,value);
-		return toReturn;
-	}
+
 }
